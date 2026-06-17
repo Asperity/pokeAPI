c404a9906978b765049ba41d4f3cb75141b9dce4
Asperity <strider1606@gmail.com>
2026-06-17 13:30:45 -0400

    Fixed math.random

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 
 async function main(){
-    const pokemonID = (Math.random()*1024)+1;
+    const pokemonID = Math.floor(Math.random()*1024)+1;
     const app = express();
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
     const data = await res.json;
-    const pokemon = data.forms.name;
+    const pokemon = data;
     console.log(pokemon);
 
 }
