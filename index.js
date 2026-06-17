e7511a24fe3ac245ecf7b68668c19af69224cb26
Asperity <strider1606@gmail.com>
2026-06-17 13:20:14 -0400

    added api fetcher methods

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 
 async function main(){
+    const pokemonID = (Math.random()*1024)+1;
     const app = express();
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemonID}')
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+    const data = await res.json;
+    const pokemon = data.forms.name;
+    console.log(pokemon);
+
 }
+main();
 
 
 
