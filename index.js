053d9218d68ac5a26f524e906ef1f987c6ee250b
Asperity <strider1606@gmail.com>
2026-06-17 13:46:29 -0400

    Added hint endpoint

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ async function main(){
     const pokemon = data.species.name;
     console.log(pokemon);
 
+    app.get("/:hint", (req,res) => {
+        const {hint} = req.params;
+        const h = Number(hint);
+    })
+
 }
 main();
 
