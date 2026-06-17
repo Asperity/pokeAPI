b98944c03ae335723fb7531e55e7c450a73e7bc2
Asperity <strider1606@gmail.com>
2026-06-17 13:35:25 -0400

    fixed the main function

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ async function main(){
     const pokemonID = Math.floor(Math.random()*1024)+1;
     const app = express();
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
-    const data = await res.json;
-    const pokemon = data;
+    const data = await res.json();
+    const pokemon = data.species.name;
     console.log(pokemon);
 
 }
