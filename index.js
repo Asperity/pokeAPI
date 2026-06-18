31d831806fb87af8c42ec40d16adc4bc634df5c5
Isaiah <i.king3458@gmail.com>
2026-06-18 10:26:26 -0400

    Set uo new route and intro to game

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import express from "express";
+import Pokedex from 'pokedex-promise-v2';
+const pokemonGuess = new Pokedex();
 
-async function main(){
+let randPokemon;
+const PORT = 3000;
+console.log("REEE")
     const pokemonID = Math.floor(Math.random()*1024)+1;
     const app = express();
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
-    const data = await res.json();
-    const pokemon = data;
-    console.log(pokemon);
-
-    app.get("/new" , (req,res) => {
-        console.log("NEW GAME!!! You have 5 correct guesses!!");
+    //const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+    // const data = await res.json();
+    // const pokemon = data;
+    // console.log(pokemon);
+    app.get("/", (req,res) => {
+        res.send("Welcome to our Pokemon Guessing game!!")
+    })
+    app.get("/new" , async  (req,res) => {
+        randPokemon = await pokemonGuess.getPokemonByName(pokemonID);
+        res.send("NEW GAME!!! You have 5 correct guesses!!");
     })
     
     app.get("/:hint", (req,res) => {
@@ -17,7 +24,8 @@ async function main(){
         const h = Number(hint);
     })
 
-}
-main();
+app.listen(PORT, () => {
+    console.log("Server started on port: " + PORT)
+})
 
 
